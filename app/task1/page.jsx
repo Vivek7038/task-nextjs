7579813ce4665db31fa5098965ecfb09064a8786
Vivek7038 <vivek888chavan@gmail.com>
2024-02-13 11:21:56 +0530

    fix

--- app/task1/page.jsx
+++ app/task1/page.jsx
@@ -12,7 +12,7 @@ import ReactFlow, {
 } from "reactflow";
 
 import "reactflow/dist/style.css";
-import { motion } from "framer-motion";}
+import { motion } from "framer-motion";
 const initialNodes = [
   { id: "A", position: { x: -99.5, y: 29 }, data: { label: "A" } },
   { id: "B", position: { x: 184, y: 34 }, data: { label: "B" } },
