644c5b90dd1e881449c96163b2d49dd91a449bb7
Vivek7038 <vivek888chavan@gmail.com>
2024-02-15 17:04:44 +0530

    initiated task1 again from scratch

--- app/task1/page.jsx
+++ app/task1/page.jsx
@@ -1,122 +1,8 @@
 "use client";
 
-import React, { useCallback, useEffect, useState } from "react";
-import ReactFlow, {
-  MiniMap,
-  Controls,
-  Background,
-  useNodesState,
-  useEdgesState,
-  addEdge,
-  MarkerType,
-} from "reactflow";
 import Link from "next/link";
-
-import "reactflow/dist/style.css";
-import { motion, useAnimation } from "framer-motion";
-import { FaFileAlt } from "react-icons/fa";
-
-const initialNodes = [
-  { id: "A", position: { x: -99.5, y: 29 }, data: { label: "A" } },
-  { id: "B", position: { x: 184, y: 34 }, data: { label: "B" } },
-  { id: "C", position: { x: 34, y: 286 }, data: { label: "C" } },
-];
-
-const initialEdges = [
-  {
-    id: "eA-B",
-    source: "A",
-    target: "B",
-    data: { label: "eA-B" },
-    style: { stroke: "black", strokeWidth: 2, curve: "curveBasis" },
-    markerEnd: {
-      type: MarkerType.Arrow,
-    },
-  },
-  {
-    id: "eB-C",
-    source: "B",
-    target: "C",
-    data: { label: "eB-C" },
-    style: { stroke: "black", strokeWidth: 2, curve: "curveBasis" },
-    markerEnd: {
-      type: MarkerType.Arrow,
-    },
-  },
-  {
-    id: "eC-A",
-    source: "C",
-    target: "A",
-    data: { label: "eC-A" },
-    style: { stroke: "black", strokeWidth: 2, curve: "curveBasis" },
-    markerEnd: {
-      type: MarkerType.Arrow,
-    },
-  },
-  {
-    id: "eA-C",
-    source: "A",
-    target: "C",
-    data: { label: "eA-C" },
-    style: { stroke: "black", strokeWidth: 2, curve: "curveBasis" },
-    markerEnd: {
-      type: MarkerType.Arrow,
-    },
-  },
-  {
-    id: "eC-B",
-    source: "C",
-    target: "B",
-    data: { label: "eC-B" },
-    style: { stroke: "black", strokeWidth: 2, curve: "curveBasis" },
-    markerEnd: {
-      type: MarkerType.Arrow,
-    },
-  },
-  {
-    id: "eB-A",
-    source: "B",
-    target: "A",
-    data: { label: "eB-A" },
-    style: { stroke: "black", strokeWidth: 2, curve: "curveBasis" },
-    markerEnd: {
-      type: MarkerType.Arrow,
-    },
-  },
-];
-
+import "./task1.css";
 export default function App() {
-  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
-  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
-  const [animationControls, setAnimationControls] = useState(null);
-  const fileIconAnimationControls = useAnimation();
-  const nodeChange = (changedNode) => {
-    console.log(
-      `Node ${changedNode.id} - X: ${changedNode.position.x}, Y: ${changedNode.position.y}`
-    );
-  };
-
-  const onNodeDragStop = (event, node) => {
-    nodeChange(node);
-  };
-
-  const onConnect = useCallback(
-    (params) => setEdges((eds) => addEdge(params, eds)),
-    [setEdges]
-  );
-
-  const animateFileIconAlongEdge = async () => {
-    await fileIconAnimationControls.start({
-      x: 78,
-      y: 89,
-      transition: { duration: 2 },
-    });
-  };
-
-  useEffect(() => {
-    animateFileIconAlongEdge();
-  }, []);
-
   return (
     <>
       <div className="pl-8 mt-4">
@@ -126,27 +12,6 @@ export default function App() {
         >
           Home
         </Link>
-      </div>
-      <div style={{ width: "100vw", height: "100vh" }}>
-        <ReactFlow
-          nodes={nodes}
-          edges={edges}
-          onNodesChange={onNodesChange}
-          onEdgesChange={onEdgesChange}
-          onConnect={onConnect}
-          onNodeDragStop={onNodeDragStop}
-          fitView
-        >
-          {/* <motion.div
-          style={{
-            position: "absolute",
-          }}
-          animate={fileIconAnimationControls}
-          className="z-[999999]"
-        >
-          <FaFileAlt size={30} color="blue" />
-        </motion.div> */}
-        </ReactFlow>
       </div>
     </>
   );
